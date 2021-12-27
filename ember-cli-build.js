ae94d8b2d4d3bb6400e6e2778f353edc5e513ff8
Robert Wagner <rwwagner90@gmail.com>
2021-12-27 12:21:55 -0500

    Start modernizing

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,8 +8,8 @@ module.exports = function() {
     snippetSearchPaths: ['app', 'tests/dummy/app', 'addon'],
     sassOptions: {
       includePaths: [
-        'bower_components/bootstrap-sass/assets/stylesheets',
-        'bower_components/flat-ui-sass/vendor/assets/stylesheets'
+        'node_modules/bootstrap-sass/assets/stylesheets',
+        'node_modules/flat-ui-sass/vendor/assets/stylesheets'
       ],
       extension: 'scss'
     }
